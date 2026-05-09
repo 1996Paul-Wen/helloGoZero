fa16ccd2660d0cd4b51954ebf5902086081af31b
sauyinman <sauyinman@sauyinmandeMacBook-Pro.local>
2026-05-09 15:50:34 +0800

    update docker cmds

--- docker_run.sh
+++ docker_run.sh
@@ -2,6 +2,7 @@
 # 镜像构建之后，运行容器
 docker run -d \
   -p 8888:8888 \
+  --add-host host.docker.internal:host-gateway \
   -v /Users/sauyinman/workspace/git_repo/helloGoZero/safebox/etc/safebox-api.yaml:/root/etc/safebox-api.yaml \
   --name safebox \
   safebox-service
--- safebox-web/docker_build.sh
+++ safebox-web/docker_build.sh
@@ -13,6 +13,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "使用 $SCRIPT_DIR 路径作为 Docker 构建上下文"
 
 docker build \
+  # --platform linux/amd64 \
   -t ${IMAGE_NAME}:${IMAGE_TAG} \
   --build-arg VITE_API_BASE_URL=/api \
   "$SCRIPT_DIR"
@@ -26,4 +27,10 @@ fi
 echo "✅ 构建完成！"
 echo ""
 echo "🚀 运行命令："
-echo "   docker run -d -p 80:80 ${IMAGE_NAME}:${IMAGE_TAG}"
+# echo "   docker run -d -p 80:80 ${IMAGE_NAME}:${IMAGE_TAG}"
+
+echo "docker run -d \
+  -p 80:80 \
+  --add-host host.docker.internal:host-gateway \
+  -v /Users/sauyinman/workspace/git_repo/helloGoZero/safebox-web/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
+  ${IMAGE_NAME}:${IMAGE_TAG}"
